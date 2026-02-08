1fa8cc000e122e177393b15fe51d53c0b2bb2bc3
alexandre <alexandre020602@gmail.com>
2026-02-08 20:43:26 -0300

    add more  one Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import "./App.css"
 import { Route, Routes } from "react-router-dom";
+import AuthProvider from "./Context/AuthContext";
+import NavBar from "./Components/NavBar/NavBar";
+
 import HomePage from "./Pages/Home/HomePage";
 import Auth from "./Pages/Auth/Auth";
 import ErrorPage from "./Pages/Error/ErrorPage";
-import NavBar from "./Components/NavBar/NavBar";
-import "./App.css"
-import AuthProvider from "./Context/AuthContext";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import Checkout from "./Pages/Checkout/Checkout";
 
 function App() {
 
@@ -16,7 +19,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/auth" element={<Auth />}></Route>
-          <Route path="/checkout"></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
+          <Route path="/products/:id" element={<ProductDetails />}></Route>
           <Route path="*" element={<ErrorPage />}></Route>
         </Routes>
       </div>
