800fed0f6fffb1e618965d285e7609325c41dde5
alexandre <alexandre020602@gmail.com>
2026-02-08 22:02:32 -0300

    cartContext

---
+++ src/Context/CartContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, useState, type ReactNode } from "react";
+
+interface CartProviderProps {
+    children: ReactNode
+}
+
+interface CartContextData {
+    addToCart: (productId: any) => void
+    cartItems: CartItem[]
+}
+
+interface CartItem {
+    id: string | number
+    quantity: number
+}
+
+const cartContent = createContext<CartContextData | null>(null)
+
+export default function CartProvider({ children }: CartProviderProps) {
+
+    const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+    const addToCart = (productId: string | number) => {
+        const existingItem = cartItems.find((item) => item.id === productId);
+        if (existingItem) {
+            const currentQuantity = existingItem.quantity;
+            const updateCartItems = cartItems.map((item) =>
+                item.id === productId
+                    ? { id: productId, quantity: currentQuantity + 1 }
+                    : item
+            )
+            setCartItems(updateCartItems)
+        }
+        else {
+            setCartItems([...cartItems, { id: productId, quantity: 1 }])
+        }
+    }
+
+    return (
+        <cartContent.Provider value={{ addToCart, cartItems }}>
+            {children}
+        </cartContent.Provider>
+    )
+}
