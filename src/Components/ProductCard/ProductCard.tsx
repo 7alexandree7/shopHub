72dc61904d6f18439f4bd78d64b6bac91d511178
alexandre <alexandre020602@gmail.com>
2026-02-08 22:48:40 -0300

    cing products components

--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom"
 import type { Product } from "../../Types/types"
+import { useCart } from "../../Hooks/useCart"
 
 
 const ProductCard = ({ product }: { product: Product }) => {
+
+    const { addToCart, cartItems } = useCart();
+    const productInCart  = cartItems.find((item) => item.id === product?.id)
+    const productQuantityLabel = productInCart ?  `(${productInCart.quantity})` : ''
+
     return (
         <div className="product-card">
-            <img src={product.image} alt={product.name} className="product-card-image" />
+            <img src={product?.image} alt={product?.name} className="product-card-image" />
             <div className="product-card-content">
-                <h3 className="product-card-name">{product.name}</h3>
-                <p className="product-card-price">${product.price}</p>
+                <h3 className="product-card-name">{product?.name}</h3>
+                <p className="product-card-price">${product?.price}</p>
                 <div className="product-card-actions">
-                    <Link to={`/products/${product.id}`} className="btn btn-secondary">View Details</Link>
-                    <button className="btn btn-primary">Add to Cart</button>
+                    <Link to={`/products/${product?.id}`} className="btn btn-secondary">View Details</Link>
+                    <button onClick={() => addToCart(product?.id)} className="btn btn-primary">Add to Cart {productQuantityLabel} </button>
                 </div>
             </div>
         </div>
--- src/Pages/ProductDetails/ProductDetails.tsx
+++ src/Pages/ProductDetails/ProductDetails.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { getProductById } from "../../data/products"
 import type { Product } from "../../Types/types"
+import { useCart } from "../../Hooks/useCart"
 
 
 const ProductDetails = () => {
@@ -10,33 +11,37 @@ const ProductDetails = () => {
     const [product, setProduct] = useState<Product | undefined>({} as Product)
     const navigate = useNavigate();
 
+    const { addToCart, cartItems } = useCart();
+    const productInCart = cartItems.find((item) => item.id === product?.id)
+    const productQuantityLabel = productInCart ? `(${productInCart.quantity})` : ''
+
     useEffect(() => {
         const foundProduct = getProductById(id)
-        
-        if(!foundProduct) {
+
+        if (!foundProduct) {
             navigate("/")
         }
 
         setProduct(foundProduct)
-    },[id])
+    }, [id])
 
-  return (
-    <div className="page">
-        <div className="container">
-            <div className="product-detail">
-                <div className="product-detail-image">
-                    <img src={product?.image} alt={product?.name} />
-                </div>
-                <div className="product-detail-content">
-                    <h1 className="product-detail-name">{product?.name}</h1>
-                    <p className="product-detail-price">${product?.price}</p>
-                    <p className="product-detail-description">{product?.description}</p>
-                    <button className="btn-btn-primary">Add to Cart</button>
+    return (
+        <div className="page">
+            <div className="container">
+                <div className="product-detail">
+                    <div className="product-detail-image">
+                        <img src={product?.image} alt={product?.name} />
+                    </div>
+                    <div className="product-detail-content">
+                        <h1 className="product-detail-name">{product?.name}</h1>
+                        <p className="product-detail-price">${product?.price}</p>
+                        <p className="product-detail-description">{product?.description}</p>
+                        <button onClick={() => addToCart(product?.id)} className="btn btn-primary">Add to Cart {productQuantityLabel} </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProductDetails
